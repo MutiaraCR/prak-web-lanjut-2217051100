b553debc6cd0ea0c680435c1bf0959df55b7cb99
MutiaraCR <mutiarainy22@gmail.com>
2024-09-27 08:39:30 +0700

    Pembaruan 4

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -13,12 +13,30 @@
     <div class="d-flex">
         <div class="form-container">
             <form action="{{ route('user.store') }}" method="POST">
+                @csrf
                 <label for="nama">Nama: </label>
-                <input type="text" name="nama" required>
+                <input type="text" name="nama">
+                @foreach($errors->get('nama') as $msg)
+                    <p class="text-danger">{{ $msg }}</p>
+                @endforeach
+
                 <label for="npm">NPM: </label>
-                <input type="text" name="npm" required>
-                <label for="kelas">Kelas: </label>
-                <input type="text" name="kelas" required>
+                <input type="text" name="npm">
+                @foreach($errors->get('npm') as $msg)
+                    <p class="text-danger">{{ $msg }}</p>
+                @endforeach
+
+                <label for="id_kelas">Kelas: </label>
+                <select name="kelas_id" id="kelas_id">
+                    <option value=""></option>
+                    @foreach($kelas as $kelasItem)
+                        <option value="{{ $kelasItem->id }}">{{ $kelasItem->nama_kelas }}</option>
+                    @endforeach
+                </select>
+                @foreach($errors->get('kelas_id') as $msg)
+                    <p class="text-danger">{{ $msg }}</p>
+                @endforeach
+
                 <button type="submit">Submit</button>
             </form>
         </div>
